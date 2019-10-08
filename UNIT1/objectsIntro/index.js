b0898e5ae8772a3c0dbfa3f50b0621a3ea39fbc6
Corey Ladovsky <coreyskymusic@gmail.com>
2019-10-07 20:24:43 -0400

    intro to objects

---
+++ UNIT1/objectsIntro/index.js
@@ -0,0 +1,30 @@
+let arr = [1, 2, 3, 4]
+// arr[7] = 6;
+// console.log(arr)
+
+// OBJECT ARE A DATA STRUCTURE THAT STORE KEY VALUE PAIRS.  
+// console.log(typeof arr)
+let fruits = ["apple", "orange", "pear"];
+                //    0         1       2 
+                // fruits[1] = "clelmentine"
+//     let fruitObj = {
+//         0: "apple",
+//         1: "orange",
+//         2: "pear"
+//     }
+//  fruitObj[1] = "clementine"
+//  fruitObj[7] = "cherries"
+//     console.log(fruitObj)
+
+let person = {
+    name: "Corey",
+    age: 2, 
+    hair: true, 
+    size: "large",
+    favoriteData: []
+}
+console.log(person["age"] = person["age"] + 1)
+
+person["married"] = true;
+
+console.log(person)
