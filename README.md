73500c4fec13cf3e60a24a10985540db23d6f508
Corey Ladovsky <coreyskymusic@gmail.com>
2019-10-12 15:46:27 -0400

    intro to functions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 All class lecture notes will be stored here.
 
 ### Class Video [playlist](https://www.youtube.com/playlist?list=PLRBt_H-AFTJYAeOFP6nXSB33VtsTJk8um)
+
+### [Highlights Doc](https://docs.google.com/document/d/1vJQab8Jx7ehoMPw2nqG7cQNvErjvZQOyLAcbVnPB6CI/edit?ts=5d8a6633)
---
+++ UNIT1/functionsIntro/index.js
@@ -0,0 +1,85 @@
+//  Write a code block that examines the sum of two arrays and 
+//  logs the array with the bigger sum. 
+
+ const arr1 = [4, 5, 26, 34, 3, 6, 7];
+ const arr2 = [5, 12, 13, 13, 15, 4, 7, 8, 9];
+
+//  let sum1 = 0;
+//  let sum2 = 0; 
+
+//  for(let i = 0; i < arr1.length; i++) {
+//      sum1 += arr1[i]
+//  }
+
+//  for(let i = 0; i < arr2.length; i++) {
+//      sum2 += arr2[i]
+//  }
+
+function sumArray(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum; 
+}
+
+
+// console.log(sum)
+
+// console.log(sumArray(arr1) === 85)
+
+// console.log(sumArray(arr1) < sumArray(arr2) ? arr1 : arr2)
+
+// console.log(Number("13"))
+
+// Function that takes in a name and returns a greeting to that name
+function greeting(name) {
+    return "Hello " + name; 
+}
+
+// console.log(greeting("Corey"))
+
+function sum(num1, num2) {
+    let sum = num1 + num2; 
+    return sum; 
+}
+// console.log(sum(10, 2))
+
+// FUNCTIONS SHOULD ONLY DO ONE THING! 
+// functions can call other functions! 
+
+// Write a function called doubleSquare that returns a number thats been doubled 
+// and then squared. Then try to break it into more than one function.
+// function printCat() {
+//     console.log("cat")
+//     return "cat"
+// }
+// printCat()
+// let count = 0; 
+function incrementCount() { // a function for it's side effect
+    count++; // count already exists in the scope that the function was declared in. 
+}
+// console.log(count)
+// incrementCount();
+// console.log(count)
+
+
+// function doubleSquare(num) {
+//     let doubled = num * 2; 
+//     return doubled * doubled ; // Math.pow(doubled, 2) // doubled**2; 
+// }
+function double(num) {
+    return num * 2; 
+}
+function square(num) {
+    return num * num; 
+}
+
+function doubleSquare(num) {
+    return square(double(num));
+}
+ console.log(doubleSquare(4))
+
+// console.log(doubleSquare(4))
+// write a function that takes in two arguments and returns the sum of them. 
+
