b8078b6fa4cea2e4bf75e42b3e992464102f30a9
Corey Ladovsky <coreyskymusic@gmail.com>
2019-10-22 20:29:07 -0400

    isValidMove

--- UNIT1/tictactoe/Board.js
+++ UNIT1/tictactoe/Board.js
@@ -14,5 +14,24 @@ class Board {
             8:[2, 1],
             9:[2, 2],
         }
+
     }
+        isValidMove(move) { // we'll get move from the user as a string number
+            if(!this.MOVES[move]) return false;
+            let [row, col] = this.MOVES[move] 
+            // let row = this.MOVES[move][0]
+            // let col = this.MOVES[move][1]
+            return typeof this.board[row][col] !== "string"
+        } 
+
+        placeMark(sym, move) {
+            // should place symbol on board at move 
+            // should decrement movesRemaining 
+        }
 }
+
+let board = new Board();
+console.log(board.isValidMove('5'))
+console.log(board.isValidMove('0'))
+console.log(board.isValidMove('a'))
+console.log(board.isValidMove('9'))
