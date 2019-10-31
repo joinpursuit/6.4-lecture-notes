878a2797df468ee7edb2f6c21ab3c801c2176440
Corey Ladovsky <coreyskymusic@gmail.com>
2019-10-30 21:13:49 -0400

    revolverClass

---
+++ UNIT1/russian_roulette/Revolver.js
@@ -0,0 +1,46 @@
+
+class Revolver {
+    constructor(chamberCount) {
+        this.chambers = new Array(chamberCount).fill(null);
+    }
+
+    addBullet() { 
+        for(let i = 0; i < this.chambers.length; i++) {
+            if(!this.chambers[i]) {
+                this.chambers[i] = true;
+                break; 
+            }
+        }
+    }
+
+    spinBarrel() {
+        let spin = Math.floor(Math.random() * this.chambers.length)
+        for(let i = 0; i < spin; i++) {
+            this.chambers.push(this.chambers.shift())
+        }
+
+    }
+
+    pullTrigger() {
+        let fired = this.chambers.shift();
+        this.chambers.push(null)
+        return fired; 
+    }
+}
+// let gun = new Revolver(6);
+// gun.addBullet();
+
+// gun.spinBarrel();
+// console.log(gun);
+// console.log(gun.pullTrigger())
+// console.log(gun);
+// console.log(gun.pullTrigger())
+// console.log(gun);
+// console.log(gun.pullTrigger())
+// gun.addBullet();
+// console.log(gun);
+// gun.spinBarrel();
+// console.log(gun);
+// gun.spinBarrel();
+// console.log(gun);
+
