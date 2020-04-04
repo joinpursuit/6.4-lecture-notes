3767b090260fbc42fae117cc4857086269acda3b
Corey Ladovsky <coreyskymusic@gmail.com>
2020-04-04 12:40:45 -0400

    add input fruit

--- UNIT5/react-redux-fruitstand/src/components/FruitStand.js
+++ UNIT5/react-redux-fruitstand/src/components/FruitStand.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import  { useParams } from 'react-router-dom'; // instead of ownProps
 import { useSelector, useDispatch } from 'react-redux';
 import { addFruit } from '../actions/fruitStandActions';
@@ -9,15 +9,31 @@ import { addFruit } from '../actions/fruitStandActions';
 
 // const FruitStand = ({ addFruit, fruits }) => { // when we had container
 const FruitStand = () => {
+    const [input, setInput] = useState("");
     const fruits = useSelector((state) => state ) // application state
     console.log("fruits", fruits)
     const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(addFruit(input))
+    }
     return(
         <div>
+            Lots and lots of fruits! 
+
+            <form onSubmit={handleSubmit}>
+                <input placeholder="fruit" 
+                        type="text" 
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}    
+                        />
+            </form>
             <button onClick={() => dispatch(addFruit("Apple"))}>Add Apple</button>
             <button onClick={() => dispatch(addFruit("Banana"))}>Add Banana</button>
-            Lots and lots of fruits! 
         </div>
+        // add a input field that will dispatch a users fruit of choice
+        //add a view for our fruits (doesnt have to be in this component)
     )
 }
 
