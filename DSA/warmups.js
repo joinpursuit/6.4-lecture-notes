a6b2eeda93f0a3db48e92c910c90b5607b39301b
Corey Ladovsky <coreyskymusic@gmail.com>
2020-09-27 14:52:29 -0400

    sameStarChar

--- DSA/warmups.js
+++ DSA/warmups.js
@@ -892,4 +892,28 @@ const majorityElement = (arr) => {
 // count = 2;
 // cand = 2
 
-console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
+// console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
+
+
+
+//PROBLEM 
+//https://the-winter.github.io/codingjs/exercise.html?name=sameStarChar&title=String-2
+// String-2 -- sameStarChar
+// Returns true if for every '*' (star) in the string, if there are chars both immediately before and after the star, they are the same.
+
+// Examples
+
+// sameStarChar('xy*yzz') → true
+// sameStarChar('xy*zzz') → false
+// sameStarChar('*xa*az') → true
+
+// iterate from 1 to str. length - 1. 
+// if we have star, check to see if chacter before and after are the same. If not the same return false. 
+// if we finish iteration, return true.
+function sameStarChar(str) {
+  for(let i = 1; i < str.length - 1; i++) {
+    let char = str[i];
+    if(char === "*" && (str[i - 1] !== str[i + 1])) return false; 
+  }
+  return true; 
+}
